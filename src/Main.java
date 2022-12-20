b4381fbd40e8c332a1da466f930966a6de6865b0
sereinme <wenhao.sereinme.li@outlook.com>
2022-12-20 13:47:13 +0800

    Game class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Game game = new Game();
+
+        JFrame window = new JFrame("2048");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.add(game);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
